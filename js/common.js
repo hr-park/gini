6e2712717ebd865cb5b5b15a8e93c703a51b83e2
hr-park <34112814+hr-park@users.noreply.github.com>
2022-12-05 21:42:30 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -11,3 +11,19 @@ $(document).ready(function(){
 		$(this).parent('.sel_box').toggleClass('on');
 	});
 });
+//팝업닫기
+function popClose(){
+	$('.pop_wrap, .dim').fadeOut('fast');
+}
+//데이터 추가 팝업
+function fncAddData(){
+	$('.pop_data_blank, .dim').show();
+}
+//팝업 퍼블 검수용
+function fncAddDataNone(){
+	$('.pop_data_none, .dim').show();
+}
+//팝업 퍼블 검수용
+function fncAddDataBefore(){
+	$('.pop_data_before, .dim').show();
+}
