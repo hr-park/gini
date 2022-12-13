399589ec785241bb621c84511953ff9d7c44e35c
hr-park <34112814+hr-park@users.noreply.github.com>
2022-12-13 12:18:53 +0900

    개발 수정 ver

--- js/common.js
+++ js/common.js
@@ -1,25 +1,51 @@
-$(document).ready(function(){
-	//alert field 내용 지우기
-	$('.inp_field .btn_del').click(function(){
+$(function(){
+	$(document).on("click", ".inp_field .btn_del", function (e) {
 		$(this).parents('.row').find('input').val('');
 		$(this).hide();
 		$(this).parents('.row').removeClass('alert');
 		$('.txt_alert').hide();
 	});
-	//selectbox
-	$('.sel_box > a').click(function(){
+
+	$(document).on("click", ".sel_box > a", function (e) {
 		$(this).parent('.sel_box').siblings('.sel_box').removeClass('on');
 		$(this).parent('.sel_box').toggleClass('on');
 	});
-	//데이터 추가 팝업 전체교재 메뉴
-	$('.pop_data .menu a.all').click(function(){
+
+	$(document).on("click", ".pop_data .menu a.all", function (e){
 		$('.menu .list_depth2').slideDown();
 	});
-	//데이터 추가 팝업 전체교재 메뉴
-	$('.pop_data .menu .list_depth1 > li > a').click(function(){
+
+	$(document).on("click", ".pop_data .menu .list_depth1 > li > a", function (e) {
 		$(this).next('.list_depth2').slideToggle();
 	});
+	
+
+	//파일선택
+	$(".file_box input[type='file']").on('change',function(){
+		var fileName = $(this).val();
+		var file = $(this).prop("files")[0];
+		var blobURL = window.URL.createObjectURL(file);
+
+		if(fileName != ""){
+			var ext = fileName.split('.').pop().toLowerCase(); //확장자분리
+			//아래 확장자가 있는지 체크
+			if($.inArray(ext, ['jpg','jpeg','gif','png', 'svg']) == -1) {
+				alert('이미지 파일만 업로드 할 수 있습니다.');
+				return;
+			}else{
+				$(this).parent('.file_box').find(".file_name").val(fileName);
+				$(this).parent('.file_box').find(".file_name").attr('readonly',true);
+				if($(this).next('.btn_preview').length < 1){
+					$(this).after('<a href="'+blobURL+'" class="btn_preview" target="_blank">파일 미리보기</a>');
+				}else{
+					$(this).next('.btn_preview').attr('href', blobURL);
+				}
+			}
+		}
+	});
+
 });
+
 //팝업 열기
 function openPop(id) {
 	$('body').css('overflow','hidden');
